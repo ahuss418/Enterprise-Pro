814c8310f6343d1443bf1e33a4e8b8e1b0fae1e7
ahuss418 <ahuss418@bradford.ac.uk>
2025-03-04 15:00:56 +0000

    Delete Code/accessibility.php

--- Code/accessibility.php
+++
@@ -1,5 +0,0 @@
-
-    <button onclick="toggleTextToSpeech(); setAccessibilityButtonTouched()">Text to Speech</button>
-    <button onclick="toggleSpeechState()">Pause/Play</button>
-    <input type="range" id="fontSizeSlider" min="10" max="24" value="16" onchange="changeFontSize()">
-    <button onclick="toggleAccessibilityPopup()">Close</button>
