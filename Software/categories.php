e1ed7c5f5942a94702cda95e98a03c0e718d1487
yasmidafuq <yabdulka@bradford.ac.uk>
2025-03-06 21:43:58 +0000

    Create categories.php

---
+++ Software/categories.php
@@ -0,0 +1,14 @@
+<?php
+require_once('includes/setup.php');
+
+$page = 'categories';
+$title = 'Categories';
+
+require_once('includes/header.php');
+
+?>
+
+<?php
+
+// Include the footer file
+require_once('includes/footer.php');
