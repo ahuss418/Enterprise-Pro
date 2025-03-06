e8f2f333c4f46a979ac56a9a77e8ec5c12383094
ahuss418 <ahuss418@bradford.ac.uk>
2025-03-06 21:44:30 +0000

    Add files via upload

---
+++ Software/assets.php
@@ -0,0 +1,14 @@
+<?php
+require_once('includes/setup.php');
+
+$page = 'assets';
+$title = 'Assets';
+
+require_once('includes/header.php');
+
+?>
+
+<?php
+
+// Include the footer file
+require_once('includes/footer.php');
---
+++ Software/reports.php
@@ -0,0 +1,14 @@
+<?php
+require_once('includes/setup.php');
+
+$page = 'reports';
+$title = 'Reports';
+
+require_once('includes/header.php');
+
+?>
+
+<?php
+
+// Include the footer file
+require_once('includes/footer.php');
